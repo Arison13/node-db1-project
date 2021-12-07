1ca3bd58f282fb7191db7da50d4208a7f311b85b
Arison13 <arisonarias@gmail.com>
2021-12-06 20:49:59 -0500

    endpoints wires and working

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,63 @@
+const Accounts = require('./accounts-model')
+
 const router = require('express').Router()
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const data = await Accounts.getAll()
+    res.json(data)
+  }
+  catch (error) {
+    next(error)
+  }
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+      console.log('get accounts by id')
+  } 
+  catch (err) {
+    next(err)
+  }
 })
 
-router.post('/', (req, res, next) => {
+router.post ('/', async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    console.log('post accounts')
+  } 
+  catch (err) {
+    next(err)
+  }
 })
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    console.log('update accounts')
+  } 
+  catch (err) {
+    next(err)
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    console.log('delete accounts')
+  } 
+  catch (err) {
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  res.status(500 || 500).json({
+    message: err.message
+  })
 })
 
 module.exports = router;
